74710a0de424339cd9b09eeb4ccceb14d64098f5
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2022-05-21 20:46:55 -0700

    Corrected mapped inputs for Orbiter

--- addons/virtualcamera/Effects/UserInput/Orbiter.gd
+++ addons/virtualcamera/Effects/UserInput/Orbiter.gd
@@ -8,6 +8,7 @@ export var negative_yaw_mapped_input : String
 export var positive_pitch_mapped_input : String
 export var negative_pitch_mapped_input : String
 export var mouse_input : bool = true
+export var mouse_sensitivity : float = 0.25
 export var rotation_speed : Vector2 = Vector2(1, 1)
 export(float, 0.0, 1.0) var lerp_speed : float = 1.0
 
@@ -22,8 +23,8 @@ onready var input_rotation : Vector2 = Vector2.ZERO
 
 func _input(event : InputEvent):
   if mouse_input and event is InputEventMouseMotion:
-    input_rotation.x -= event.relative.x * rotation_speed.x
-    input_rotation.y += event.relative.y * rotation_speed.y
+    input_rotation.x -= event.relative.x * rotation_speed.x * mouse_sensitivity
+    input_rotation.y += event.relative.y * rotation_speed.y * mouse_sensitivity
 
 func _physics_process(delta : float):
   if orbit_radii.x < 0.0:
@@ -35,18 +36,18 @@ func _physics_process(delta : float):
   if Engine.editor_hint:
     translation = Vector3.ZERO
   else:
+    var yaw_axis = 0.0
+    if !negative_yaw_mapped_input.empty():
+      yaw_axis -= Input.get_action_strength(negative_yaw_mapped_input)
+    if !positive_yaw_mapped_input.empty():
+      yaw_axis += Input.get_action_strength(positive_yaw_mapped_input)
+    input_rotation.x += yaw_axis * rotation_speed.x * 5
     var pitch_axis = 0.0
     if !negative_pitch_mapped_input.empty():
       pitch_axis -= Input.get_action_strength(negative_pitch_mapped_input)
     if !positive_pitch_mapped_input.empty():
       pitch_axis += Input.get_action_strength(positive_pitch_mapped_input)
-    input_rotation.x += pitch_axis * rotation_speed.x
-    var yaw_axis = 0.0
-    if !negative_yaw_mapped_input.empty():
-      yaw_axis -= Input.get_action_strength(negative_yaw_mapped_input)
-    if !positive_yaw_mapped_input.empty():
-      pitch_axis += Input.get_action_strength(positive_yaw_mapped_input)
-    input_rotation.y += yaw_axis * rotation_speed.y
+    input_rotation.y += pitch_axis * rotation_speed.y * 5
     
     input_rotation.x = fmod(input_rotation.x + 180, 360) - 180
     input_rotation.y = clamp(input_rotation.y, -90, 90)
