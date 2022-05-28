20b3d0df253e0446360520e7728168fbcf140943
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2022-05-28 12:23:37 +0300

    Allow locking axes on Follow modifier
    This functionality is used for sidescroller (2.5D) and top-down views.

--- addons/virtualcamera/TransformModifiers/Follow.gd
+++ addons/virtualcamera/TransformModifiers/Follow.gd
@@ -6,6 +6,9 @@ class_name Follow
 export var follow_target : NodePath
 export(float, 0.0, 1.0) var follow_lerp_t : float = 1.0
 export var follow_offset : Vector3 = Vector3.ZERO
+export var lock_x : bool = false
+export var lock_y : bool = false
+export var lock_z : bool = false
 
 func has_follow_target() -> bool:
 	return not follow_target.is_empty()
@@ -14,6 +17,11 @@ func _physics_process(delta : float):
 	if not self.follow_target.is_empty():
 		var target = get_node_or_null(self.follow_target)
 		if target != null:
-			global_transform.origin = global_transform.origin.linear_interpolate(target.global_transform.origin + self.follow_offset, self.follow_lerp_t)
+			if not self.lock_x:
+				global_transform.origin.x = lerp(global_transform.origin.x, target.global_transform.origin.x + self.follow_offset.x, self.follow_lerp_t)
+			if not self.lock_y:
+				global_transform.origin.y = lerp(global_transform.origin.y, target.global_transform.origin.y + self.follow_offset.y, self.follow_lerp_t)
+			if not self.lock_z:
+				global_transform.origin.z = lerp(global_transform.origin.z, target.global_transform.origin.z + self.follow_offset.z, self.follow_lerp_t)
 		else:
 			self.follow_target = NodePath()
