41efca0b790a17b3aa4ec3c3f4e6def254f2891a
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2022-05-28 07:30:47 +0300

    Fix mixed spaces and tabs

--- addons/virtualcamera/plugin.gd
+++ addons/virtualcamera/plugin.gd
@@ -4,9 +4,9 @@ extends EditorPlugin
 var http : HTTPRequest = null
 
 func _enter_tree():
- 	http = HTTPRequest.new()
- 	add_child(http)
- 	http.request("https://pluginstats.brycedixon.dev/", [], true, HTTPClient.METHOD_POST, JSON.print({plugin="VCamera", project=ProjectSettings.get_setting("application/config/name")}))
+	http = HTTPRequest.new()
+	add_child(http)
+	http.request("https://pluginstats.brycedixon.dev/", [], true, HTTPClient.METHOD_POST, JSON.print({plugin="VCamera", project=ProjectSettings.get_setting("application/config/name")}))
 
 func _exit_tree():
 	pass
