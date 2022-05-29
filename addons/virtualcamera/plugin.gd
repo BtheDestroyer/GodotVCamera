fb57af9c84e782045b1acb3a126007dd8d83569e
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2022-05-28 18:11:31 -0700

    Hash project name before sending to usage tracking server

--- addons/virtualcamera/plugin.gd
+++ addons/virtualcamera/plugin.gd
@@ -6,7 +6,8 @@ func _enter_tree():
 	# See: https://github.com/BtheDestroyer/GodotVCamera#privacy-notice
 	var http = HTTPRequest.new()
 	add_child(http)
-	http.request("https://pluginstats.brycedixon.dev/", [], true, HTTPClient.METHOD_POST, JSON.print({plugin="VCamera", project=ProjectSettings.get_setting("application/config/name")}))
+	var project_hash = ProjectSettings.get_setting("application/config/name").sha256_text()
+	http.request("https://pluginstats.brycedixon.dev/", [], true, HTTPClient.METHOD_POST, JSON.print({plugin="VCamera", project=project_hash}))
 	# Usage Tracking
 
 func _exit_tree():
