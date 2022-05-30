fee7e1180060cca7c1da721332871bc8303d4315
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2022-05-30 21:56:56 +0300

    Remove redundant variable prefixes (shake_)

--- addons/virtualcamera/TransformModifiers/Shake.gd
+++ addons/virtualcamera/TransformModifiers/Shake.gd
@@ -4,10 +4,10 @@ extends "res://addons/virtualcamera/TransformModifiers/TransformModifier.gd"
 class_name Shake
 
 export var noise : OpenSimplexNoise
-export var shake_speed : float = 1.0
-export var shake_strength_translation : Vector3 = Vector3.ZERO
-export var shake_strength_rotation : Vector3 = Vector3.ZERO
-var shake_time : float = 0.0
+export var speed : float = 1.0
+export var strength_translation : Vector3 = Vector3.ZERO
+export var strength_rotation : Vector3 = Vector3.ZERO
+var time : float = 0.0
 
 func _ready():
 	if not noise:
@@ -18,15 +18,15 @@ func _ready():
 
 func _physics_process(delta : float):
 	if noise:
-		shake_time += delta * self.shake_speed * 100
+		time += delta * self.speed * 100
 		translation = Vector3(
-			noise.get_noise_1d(shake_time) * self.shake_strength_translation.x,
-			noise.get_noise_1d(shake_time - 10000) * self.shake_strength_translation.y,
-			noise.get_noise_1d(shake_time - 20000) * self.shake_strength_translation.z
+			noise.get_noise_1d(time) * self.strength_translation.x,
+			noise.get_noise_1d(time - 10000) * self.strength_translation.y,
+			noise.get_noise_1d(time - 20000) * self.strength_translation.z
 			)
 		rotation = Vector3(
-			deg2rad(noise.get_noise_1d(shake_time - 30000) * self.shake_strength_rotation.x),
-			deg2rad(noise.get_noise_1d(shake_time - 40000) * self.shake_strength_rotation.y),
-			deg2rad(noise.get_noise_1d(shake_time - 50000) * self.shake_strength_rotation.z)
+			deg2rad(noise.get_noise_1d(time - 30000) * self.strength_rotation.x),
+			deg2rad(noise.get_noise_1d(time - 40000) * self.strength_rotation.y),
+			deg2rad(noise.get_noise_1d(time - 50000) * self.strength_rotation.z)
 			)
 
