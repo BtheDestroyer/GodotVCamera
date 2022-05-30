d5ce6b3123b13a8ee4e984191baf6daf87f576ae
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2022-05-30 22:17:48 +0300

    Add intensity, automatic intensity decrease

--- addons/virtualcamera/TransformModifiers/Shake.gd
+++ addons/virtualcamera/TransformModifiers/Shake.gd
@@ -5,8 +5,14 @@ class_name Shake
 
 export var noise : OpenSimplexNoise
 export var speed : float = 1.0
+
+export(float, 0, 1) var intensity : float = 1.0
+export(float, EASE) var intensity_curve : float = 2.0
+export var intensity_decrease_rate : float = 0.0
+
 export var strength_translation : Vector3 = Vector3.ZERO
 export var strength_rotation : Vector3 = Vector3.ZERO
+
 var time : float = 0.0
 
 func _ready():
@@ -16,17 +22,19 @@ func _ready():
 		noise.octaves = 4
 		noise.persistence = 0.8
 
+const DEG2RAD = TAU / 360
+
 func _physics_process(delta : float):
 	if noise:
-		time += delta * self.speed * 100
-		translation = Vector3(
-			noise.get_noise_1d(time) * self.strength_translation.x,
-			noise.get_noise_1d(time - 10000) * self.strength_translation.y,
-			noise.get_noise_1d(time - 20000) * self.strength_translation.z
-			)
-		rotation = Vector3(
-			deg2rad(noise.get_noise_1d(time - 30000) * self.strength_rotation.x),
-			deg2rad(noise.get_noise_1d(time - 40000) * self.strength_rotation.y),
-			deg2rad(noise.get_noise_1d(time - 50000) * self.strength_rotation.z)
-			)
+		time += delta * speed * 100
+		
+		if not Engine.editor_hint:
+			intensity = max(0, intensity - intensity_decrease_rate * delta)
+		var amplitude = ease(intensity, intensity_curve)
+		
+		translation = Vector3(noise.get_noise_1d(time), noise.get_noise_1d(time - 10000), noise.get_noise_1d(time - 20000))
+		translation *= strength_translation * amplitude
+		
+		rotation = Vector3(noise.get_noise_1d(time - 30000), noise.get_noise_1d(time - 40000), noise.get_noise_1d(time - 50000))
+		rotation *= strength_rotation * DEG2RAD * amplitude
 
