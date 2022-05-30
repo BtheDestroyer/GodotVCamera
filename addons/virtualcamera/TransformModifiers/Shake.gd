c4cffd3dd3523a0ce24ddcb2b631f90922bd2408
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2022-05-30 21:42:55 +0300

    Change shake noise to export variable and make shake tool script
    Note: breaking change: shake now does allow positioning itself. Set position via parent object instead. This prevents floating point drifting and similiar behaviour was already with rotation.

--- addons/virtualcamera/TransformModifiers/Shake.gd
+++ addons/virtualcamera/TransformModifiers/Shake.gd
@@ -1,32 +1,31 @@
+tool
 extends "res://addons/virtualcamera/TransformModifiers/TransformModifier.gd"
 
 class_name Shake
 
-onready var noise = OpenSimplexNoise.new()
+export var noise : OpenSimplexNoise
 export var shake_speed : float = 1.0
 export var shake_strength_translation : Vector3 = Vector3.ZERO
 export var shake_strength_rotation : Vector3 = Vector3.ZERO
 var shake_time : float = 0.0
 
 func _ready():
-	noise.seed = get_instance_id()
-	noise.octaves = 4
-	noise.period = 20.0
-	noise.persistence = 0.8
-
-onready var base_translation = global_transform.origin
+	if not noise:
+		noise = OpenSimplexNoise.new()
+		noise.seed = get_instance_id()
+		noise.octaves = 4
+		noise.persistence = 0.8
 
 func _physics_process(delta : float):
-	shake_time += delta * self.shake_speed * 50
-	var translation_since_last_process = global_transform.origin - base_translation
-	translation = base_translation + translation_since_last_process + Vector3(
-		noise.get_noise_4d(shake_time, 0, 0, 0) * self.shake_strength_translation.x,
-		noise.get_noise_4d(0, shake_time, 0, 0) * self.shake_strength_translation.y,
-		noise.get_noise_4d(0, 0, shake_time, 0) * self.shake_strength_translation.z
-		)
-	base_translation = global_transform.origin
-	rotation = Vector3(
-		deg2rad(noise.get_noise_4d(shake_time, 0, 0, 1) * self.shake_strength_rotation.x),
-		deg2rad(noise.get_noise_4d(0, shake_time, 0, 1) * self.shake_strength_rotation.y),
-		deg2rad(noise.get_noise_4d(0, 0, shake_time, 1) * self.shake_strength_rotation.z)
-		)
+	if noise:
+		shake_time += delta * self.shake_speed * 100
+		translation = Vector3(
+			noise.get_noise_4d(shake_time, 0, 0, 0) * self.shake_strength_translation.x,
+			noise.get_noise_4d(0, shake_time, 0, 0) * self.shake_strength_translation.y,
+			noise.get_noise_4d(0, 0, shake_time, 0) * self.shake_strength_translation.z
+			)
+		rotation = Vector3(
+			deg2rad(noise.get_noise_4d(shake_time, 0, 0, 1) * self.shake_strength_rotation.x),
+			deg2rad(noise.get_noise_4d(0, shake_time, 0, 1) * self.shake_strength_rotation.y),
+			deg2rad(noise.get_noise_4d(0, 0, shake_time, 1) * self.shake_strength_rotation.z)
+			)
